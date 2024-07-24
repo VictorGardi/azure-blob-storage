4aa3be22262de609b9bfaa6093f7c6d880869a9e
Victor <victor.olssn@gmail.com>
2024-07-24 08:14:16 +0200

    Revert "add service call argument to sync func"
    This reverts commit 2e54295f6ee80c678e44d26b9094cd6de2c15ddc.

--- custom_components/azure_blob_sync/__init__.py
+++ custom_components/azure_blob_sync/__init__.py
@@ -1,7 +1,6 @@
 import logging
-from typing import Optional
 from datetime import timedelta
-from homeassistant.core import HomeAssistant, ServiceCall
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.event import (
     async_track_state_change,
     async_track_time_interval,
@@ -9,6 +8,7 @@ from homeassistant.helpers.event import (
 import voluptuous as vol
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.typing import ConfigType
+
 from .const import (
     DOMAIN,
     CONF_CONNECTION_STRING,
@@ -45,53 +45,53 @@ CONFIG_SCHEMA = vol.Schema(
     },
     extra=vol.ALLOW_EXTRA,
 )
-
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up Azure Blob Sync from YAML configuration."""
-    _LOGGER.info("Setting up azure blob sync")
+    _LOGGER.debug("Starting async_setup for azure_blob_sync")
     conf = config.get(DOMAIN)
     if conf is None:
+        _LOGGER.error("No configuration found for azure_blob_sync")
         return False
-
-    azure_blob_sync = AzureBlobSync(conf[CONF_CONNECTION_STRING])
-    _LOGGER.info("AzureBlobSync class initialized")
-
-    async def sync_folders(call: Optional[ServiceCall]):
+    
+    _LOGGER.debug("Configuration found: %s", conf)
+    
+    try:
+        azure_blob_sync = AzureBlobSync(conf[CONF_CONNECTION_STRING])
+        _LOGGER.debug("AzureBlobSync instance created successfully")
+    except Exception as e:
+        _LOGGER.error("Failed to create AzureBlobSync instance: %s", str(e))
+        return False
+    async def sync_folders():
         """Sync folders."""
-        _LOGGER.info("Starting folder sync")
-        try:
-            await azure_blob_sync.create_container(conf[CONF_CONTAINER_NAME])
-            await azure_blob_sync.sync_folder_to_blob(
-                conf[CONF_CONTAINER_NAME], conf[CONF_LOCAL_FOLDER], conf[CONF_BLOB_FOLDER]
-            )
-            _LOGGER.info("Folder sync completed successfully")
-        except Exception as e:
-            _LOGGER.error("Error during folder sync: %s", str(e))
+        await azure_blob_sync.create_container(conf[CONF_CONTAINER_NAME])
+        await azure_blob_sync.sync_folder_to_blob(
+            conf[CONF_CONTAINER_NAME], conf[CONF_LOCAL_FOLDER], conf[CONF_BLOB_FOLDER]
+        )
 
     hass.services.async_register(DOMAIN, "sync", sync_folders)
-    _LOGGER.info("Registered 'sync' service")
+    _LOGGER.info("registered service")
 
     sync_modes = conf[CONF_SYNC_MODES]
+
     if SYNC_MODE_SCHEDULE in sync_modes and CONF_SYNC_INTERVAL in conf:
         interval = timedelta(minutes=conf[CONF_SYNC_INTERVAL])
-        async_track_time_interval(hass, lambda _: sync_folders(None), interval)
-        _LOGGER.info("Scheduled sync set up with interval: %s minutes", conf[CONF_SYNC_INTERVAL])
+        async_track_time_interval(hass, sync_folders, interval)
 
-    if (
-        SYNC_MODE_EVENT in sync_modes
-        and CONF_TRIGGER_ENTITY in conf
-        and CONF_TRIGGER_STATE in conf
-    ):
-        entity_id = conf[CONF_TRIGGER_ENTITY]
-        trigger_state = conf[CONF_TRIGGER_STATE]
+    #if (
+    #    SYNC_MODE_EVENT in sync_modes
+    #    and CONF_TRIGGER_ENTITY in conf
+    #    and CONF_TRIGGER_STATE in conf
+    #):
+    #    entity_id = conf[CONF_TRIGGER_ENTITY]
+    #    trigger_state = conf[CONF_TRIGGER_STATE]
 
-        async def state_change_listener(entity_id, old_state, new_state):
-            if new_state.state == trigger_state:
-                await sync_folders(None)
+    #    async def state_change_listener(new_state):
+    #        if new_state.state == trigger_state:
+    #            await sync_folders()
 
-        async_track_state_change(hass, entity_id, state_change_listener)
-        _LOGGER.info("Event-based sync set up for entity: %s, trigger state: %s", entity_id, trigger_state)
+    #    async_track_state_change(hass, entity_id, state_change_listener)
 
     return True
